89eaa403c39d82e64e0d392e27c97f0dd6ef71f0
Abu Naim <abunaim140@gmail.com>
2024-08-23 01:21:36 +0600

    implement less code in banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -2,28 +2,34 @@ import manBg from '../../../assets/man.png'
 import womenBg from '../../../assets/women.png'
 import { Button } from 'antd';
 
+const banners = [
+    {
+        title: 'Fashion For Her',
+        subtitle: 'New Arrival',
+        img: womenBg
+    },
+    {
+        title: 'Fashion For Him',
+        subtitle: 'New Arrival',
+        img: manBg
+    }
+];
+
 const Banner = () => {
     return (
         <div className='h-[120vh]'>
             <div className='flex h-full text-white'>
-                <div className='flex items-center h-full bg-no-repeat bg-center bg-cover object-contain flex-1' style={{ backgroundImage: `url(${womenBg})` }}>
-                    <div className='px-16 w-full'>
-                        <h3 className='text-2xl font-bold'>New Arrival</h3>
-                        <h1 className='text-7xl mt-4 mb-7'>Fashion For Her</h1>
-                        <Button className='rounded-none px-14 py-7' size={'large'}>
-                            Shop Now
-                        </Button>
-                    </div>
-                </div>
-                <div className='flex items-center h-full bg-no-repeat bg-center  bg-cover object-contain flex-1' style={{ backgroundImage: `url(${manBg})` }}>
-                    <div className='px-16 w-full'>
-                        <h3 className='text-2xl font-bold'>New Arrival</h3>
-                        <h1 className='text-7xl mt-4 mb-7'>Fashion For Him</h1>
-                        <Button className='rounded-none px-14 py-7' size={'large'}>
-                            Shop Now
-                        </Button>
-                    </div>
-                </div>
+                {
+                    banners.map((banner, idx) => <div key={idx} className='flex items-center h-full bg-no-repeat bg-center bg-cover object-contain flex-1' style={{ backgroundImage: `url(${banner.img})` }}>
+                        <div className='px-16 w-full'>
+                            <h3 className='text-2xl font-bold'>{banner.subtitle}</h3>
+                            <h1 className='text-7xl mt-4 mb-7'>{banner.title}</h1>
+                            <Button className='rounded-none px-14 py-7' size={'large'}>
+                                Shop Now
+                            </Button>
+                        </div>
+                    </div>)
+                }
             </div>
         </div>
     );
