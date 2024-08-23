80cd706e6e3df6d5325d9fc4723c9005ef35e87a
Abu Naim <abunaim140@gmail.com>
2024-08-23 23:30:24 +0600

    added product card and product files and displey new products

---
+++ src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,29 @@
+import { Card, Image } from 'antd';
+import { FiShoppingCart } from 'react-icons/fi';
+
+const ProductCard = ({ product }) => {
+    const {name, image, price} = product;
+
+    return (
+        <Card
+            className='border-none cursor-pointer rounded-none'
+            styles={{
+                body: { padding: 0 }
+            }}
+            cover={<Image preview={false} alt="example" className='border-[1px]' style={{ borderRadius: 0 }} src={image} />}
+        >
+            <div className='flex items-center justify-between border-[1px] p-4 mt-5'>
+                <div>
+                    <h1 className='text-lg hover:text-[#00BADB] cursor-pointer'>{name}</h1>
+                    <h3 className='text-base'>&#2547;{price}</h3>
+                </div>
+                <div className='flex items-center gap-3 text-xl'>
+                    <div className='border-r-[1px] h-10' />
+                    <FiShoppingCart />
+                </div>
+            </div>
+        </Card>
+    );
+};
+
+export default ProductCard;
--- src/Pages/Home/NewProducts/NewProducts.jsx
+++ src/Pages/Home/NewProducts/NewProducts.jsx
@@ -1,33 +1,28 @@
 import { FiShoppingCart } from "react-icons/fi";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Card, Image } from 'antd';
+import { useEffect, useState } from "react";
+import ProductCard from "../../../Components/ProductCard/ProductCard";
 
 const NewProducts = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
     return (
         <div className='mb-10 px-16'>
             <SectionTitle
                 title='New Arrivals'
                 description='Here’s some of our most popular products people are in love with.'
             />
-            <div className='grid grid-cols-5 gap-5 mt-12'>
-                <Card
-                    className='border-none cursor-pointer rounded-none'
-                    styles={{
-                        body: { padding: 0, }
-                    }}
-                    cover={<Image alt="example" className='border-[1px]' style={{ borderRadius: 0,  }} src="https://i.postimg.cc/wjXSCGDx/Untitled-design-3.png" />}
-                >
-                    <div className='flex items-center justify-between border-[1px] p-4 mt-5'>
-                        <div>
-                            <h1 className='text-lg hover:text-[#00BADB] cursor-pointer'>Sunsets Lotus Kauai Keyhole</h1>
-                            <h3 className='text-base'>$550</h3>
-                        </div>
-                        <div className='flex items-center gap-3 text-xl'>
-                            <div className='border-r-[1px] h-12' />
-                            <FiShoppingCart />
-                        </div>
-                    </div>
-                </Card>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 mt-12'>
+                {
+                    products.map((product, idx) => <ProductCard key={idx} product={product} />)
+                }
             </div>
         </div>
     );
